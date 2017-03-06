afb207ce74393a125b8b5c642077a99d2c58686d
Stefan Kajkut <stefan.kajkutsf@gmail.com>
2017-03-06 18:13:34 +0100

    Model ucenik

--- src/model/PohadjanjePredmeta.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-/**
- * Created by stefan on 3/6/17.
- */
-public class PohadjanjePredmeta {
-}
--- src/model/Ucenik.java
+++ src/model/Ucenik.java
@@ -1,7 +1,48 @@
 package model;
 
+import java.util.List;
+
 /**
  * Created by alligator on 6.3.17..
  */
 public class Ucenik extends Korisnik {
+
+    private List<Predmet> predmeti;
+    private List<Dokument> dokumenti;
+    private String biografija;
+
+    public Ucenik() {}
+
+    public Ucenik(String ime, String prezime, String email, String password,
+                  Uloga uloga, List<Predmet> predmeti,
+                  List<Dokument> dokumenti, String biografija) {
+        super(ime, prezime, email, password, uloga);
+        this.predmeti = predmeti;
+        this.dokumenti = dokumenti;
+        this.biografija = biografija;
+    }
+
+    public List<Predmet> getPredmeti() {
+        return predmeti;
+    }
+
+    public void setPredmeti(List<Predmet> predmeti) {
+        this.predmeti = predmeti;
+    }
+
+    public List<Dokument> getDokumenti() {
+        return dokumenti;
+    }
+
+    public void setDokumenti(List<Dokument> dokumenti) {
+        this.dokumenti = dokumenti;
+    }
+
+    public String getBiografija() {
+        return biografija;
+    }
+
+    public void setBiografija(String biografija) {
+        this.biografija = biografija;
+    }
 }
